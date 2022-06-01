3f84e377be29daa5a66984e5719836a821907235
Nguyễn Cát Tiên <tiennc@gtv.vn>
2022-06-02 01:45:40 +0700

    remove

--- opensearch/conf.go
+++ opensearch/conf.go
@@ -1,13 +1,10 @@
 package opensearch
 
 import (
-    "encoding/json"
     "fmt"
-    "github.com/spf13/viper"
     "github.com/Kephas73/go-lib/constant"
-    "github.com/Kephas73/go-lib/util"
+    "github.com/spf13/viper"
     "strings"
-    "time"
 )
 
 type OpenSearchConfig struct {
@@ -52,63 +49,4 @@ func createConfigFromEnv(configKeys ...string) {
         openSearchConf.IndexFormat = constant.IndexDefault
     }
 
-}
-
-type DataLogBase interface {
-    FromJSON(data []byte) error
-    ToJSON() string
-    SetEventName(eventName string) DataLogBase
-    SetLogDataJSON(dataJSON string) DataLogBase
-    SetDescription(description string) DataLogBase
-}
-
-// DefaultDataLog type;
-type DefaultDataLog struct {
-    EventName    string      `json:"event_name,omitempty"`
-    LogDataJSON  interface{} `json:"log_data_json,omitempty"`
-    Description  string      `json:"description,omitempty"`
-    TimeStarted  int64       `json:"time_started,omitempty"`
-    TimeFinished int64       `json:"time_finished,omitempty"`
-    TimeExecute  int64       `json:"time_execute,omitempty"`
-}
-
-// FromJSON func;
-func (obj *DefaultDataLog) FromJSON(data []byte) error {
-    return json.Unmarshal(data, obj)
-}
-
-// ToJSON func;
-func (obj *DefaultDataLog) ToJSON() string {
-    return util.JSONDebugDataString(obj)
-}
-
-// SetEventName func;
-func (obj *DefaultDataLog) SetEventName(eventName string) DataLogBase {
-    obj.EventName = eventName
-
-    return obj
-}
-
-// SetLogDataJSON func;
-func (obj *DefaultDataLog) SetLogDataJSON(dataJSON string) DataLogBase {
-    obj.LogDataJSON = dataJSON
-
-    return obj
-}
-
-// SetDescription func;
-func (obj *DefaultDataLog) SetDescription(description string) DataLogBase {
-    obj.Description = description
-
-    return obj
-}
-
-func NewDefaultDataLog() *DefaultDataLog {
-    timeNow := time.Now().Unix()
-    return &DefaultDataLog{
-        EventName:    "DefaultLog",
-        TimeStarted:  timeNow,
-        TimeFinished: timeNow,
-        TimeExecute:  0,
-    }
 }
