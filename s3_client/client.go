a2fe87c84ed8770c421216b940c903830469b3f4
Kephas73 <45686868+Kephas73@users.noreply.github.com>
2022-06-13 17:44:49 +0700

    Update client.go

--- s3_client/client.go
+++ s3_client/client.go
@@ -4,7 +4,6 @@ import (
     "fmt"
     "github.com/Kephas73/go-lib/logger"
 
-    "github.com/Kephas73/go-lib/constant"
     "github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/aws/credentials"
     "github.com/aws/aws-sdk-go/aws/session"
